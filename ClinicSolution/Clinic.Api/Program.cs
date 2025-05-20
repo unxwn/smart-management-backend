9fe80708eaf06fab64511654d4de19008da0cd7c
Myroslav Polishchuk <myrospolil@gmail.com>
2025-05-20 19:49:12 +0100

    Change Program.cs

--- ClinicSolution/Clinic.Api/Program.cs
+++ ClinicSolution/Clinic.Api/Program.cs
@@ -78,11 +78,14 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
