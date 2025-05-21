b0af562a4dc33e5e32a8760ad49c6854ea7b22f9
Myroslav Polishchuk <myrospolil@gmail.com>
2025-05-21 19:51:47 +0100

    Add mapping profile for Automapper

---
+++ ClinicSolution/Clinic.Api/Profiles/MappingProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Clinic.Domain.Models.DTOs;
+using Clinic.Domain.Models.Entities;
+
+namespace Clinic.Api.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Product, ProductDto>().ReverseMap();
+            CreateMap<Supplier, SupplierDto>().ReverseMap();
+            CreateMap<Patient, PatientDto>().ReverseMap();
+            CreateMap<Doctor, DoctorDto>().ReverseMap();
+            CreateMap<Visit, VisitDto>().ReverseMap();
+            CreateMap<Appointment, AppointmentDto>().ReverseMap();
+            CreateMap<Prescription, PrescriptionDto>().ReverseMap();
+            CreateMap<Payment, PaymentDto>().ReverseMap();
+            CreateMap<MedicalRecord, MedicalRecordDto>().ReverseMap();
+
+            CreateMap<User, UserDto>();
+            CreateMap<AuthUserDto, User>();
+        }
+    }
+}
